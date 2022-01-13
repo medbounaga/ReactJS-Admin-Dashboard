69bc88eb8b347f562a72070aa93f85b345446ed1
Mohammed Bounaga <medbounaga@gmail.com>
2022-01-13 15:31:51 +0100

    LineChart Component cleanup

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,84 +1,114 @@
 import React from 'react';
 
 import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
-import faker from 'faker';
 
 ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
 );
 
-export const options = {
+export function LineChart({ data, height = '270px', width = '100%' }) {
+  const labels = data.labels;
+
+  const colors = [
+    'rgb(255, 99, 132)',
+    'rgb(75, 192, 192)',
+    'rgb(53, 162, 235)',
+  ];
+
+  const datasets = data.stats.map((stat, index) => {
+    return {
+      label: stat.label,
+      data: stat.data,
+      backgroundColor: colors[index],
+      borderColor: colors[index],
+    };
+  });
+
+  const options = {
+    maintainAspectRatio: false,
     responsive: true,
     plugins: {
-        legend: {
-            display: true,
-            position: 'top',
-            labels: {
-                fontColor: '#333',
-                usePointStyle: true,
-                pointStyle: 'circle',
-            }
+      legend: {
+        display: true,
+        position: 'top',
+        labels: {
+          fontColor: '#333',
+          usePointStyle: true,
+          pointStyle: 'circle',
         },
-        title: {
-            display: false,
-            text: '',
-        },
+      },
+      title: {
+        display: false,
+        text: '',
+      },
     },
     elements: {
-        line: {
-            tension: 0.2
-        }
+      line: {
+        tension: 0.2,
+      },
+      point: {
+        hitRadius: 1,
+      },
     },
-};
+    scales: {
+      x: {
+        grid: {
+          display: true,
+          drawBorder: true,
+          drawOnChartArea: true,
+          drawTicks: true,
+          color: 'rgb(249 249 249)',
+        },
+      },
+      y: {
+        beginAtZero: true,
+        grid: {
+          display: true,
+          drawBorder: false,
+          color: function (context) {
+            if (context.tick.value >= 0) {
+              return 'rgb(243 243 243)';
+            } else if (context.tick.value < 0) {
+              return 'rgb(243 243 243)';
+            }
 
-const labels = ['January', 'February', 'March', 'April', 'May', 'June'];
+            return '#000000';
+          },
+        },
+      },
+    },
 
-export const data = {
+    interaction: {
+      intersect: false,
+      mode: 'index',
+      axis: 'x',
+    },
+  };
+
+  const formateddata = {
     labels,
-    datasets: [
-        {
-            label: 'Sales',
-            data: labels.map(() => faker.datatype.number({ min: 100, max: 1000 })),
-            borderColor: 'rgb(255, 99, 132)',
-            backgroundColor: 'rgb(255, 99, 132)',
-        },
-        {
-            label: 'Profit',
-            data: labels.map(() => faker.datatype.number({ min: 100, max: 1000 })),
-            borderColor: 'rgb(75, 192, 192)',
-            backgroundColor: 'rgb(75, 192, 192)',
-        },
-        {
-            label: 'Cogs',
-            data: labels.map(() => faker.datatype.number({ min: 100, max: 1000 })),
-            borderColor: 'rgb(53, 162, 235)',
-            backgroundColor: 'rgb(53, 162, 235)',
+    datasets,
+  };
 
-        },
-    ],
-};
-
-
-export function LineChart({ height = '270px', width = '100%' }) {
-    return (
-        <div style={{ height: height, width: width }}>
-            <Line options={options} data={data} />
-        </div>
-    )
+  return (
+    <div style={{ height: height, width: width }}>
+      <Line options={options} data={formateddata} />
+    </div>
+  );
 }
