d7255de0657795a5183189956c3f54d526918a56
Mohammed Bounaga <medbounaga@gmail.com>
2022-01-19 14:56:44 +0100

    ListGroup component cleanup

--- src/components/ListGroup.jsx
+++ src/components/ListGroup.jsx
@@ -1,15 +1,25 @@
 import styles from "../styles/ListGroup.module.scss";
 
-export function ListGroup({ children, ...restProps }) {
+
+
+export function ListGroup({ colored, hasBorder, children, ...restProps }) {
+
+    const listGroupClasses = [styles.listGroup,
+                       colored && styles.colored,
+                    hasBorder && styles.hasBorder].join(" ");
+
     return (
-        <ul className={styles.listGroup} {...restProps}>
+        <ul className={ listGroupClasses }
+            {...restProps} >
+
             {children}
+
         </ul>
     )
 }
 
 
-ListGroup.Item =  ({ children, ...restProps }) => {
+ListGroup.Item = ({ children, ...restProps }) => {
     return (
         <li className={styles.listGroupItem} {...restProps}>
             {children}
