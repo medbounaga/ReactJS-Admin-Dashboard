e75567fbb501a18c98c2817a50662c2916b7b807
Mohammed Bounaga <medbounaga@gmail.com>
2022-02-03 00:20:34 +0100

    Sales vs Profit Container (Bar and Line charts)
    Display Sales vs Profit by period with bar and line charts

---
+++ src/containers/SalesProfit.jsx
@@ -0,0 +1,177 @@
+import '../styles/globals.scss';
+import { useState, useEffect } from 'react';
+import api from '../api/analytics';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+import { Card, ButtonGroup, BarChart, LineChart } from '../components';
+import { AiOutlineBarChart, AiOutlineLineChart } from 'react-icons/ai';
+import dates from '../utils/dates';
+
+export function SalesProfit() {
+  const endPoints = [
+    { url: 'salesAmount', name: 'Sales' },
+    { url: 'profit', name: 'Gross Profit' },
+    //  { url: "cogs", name: "COGS" },
+  ];
+
+  const charts = {
+    BAR: 'bar',
+    LINE: 'line',
+  };
+
+  const chartSwitchButtons = [
+    { label: <AiOutlineBarChart />, value: charts.BAR },
+    { label: <AiOutlineLineChart />, value: charts.LINE },
+  ];
+
+  const intervals = {
+    DAY: 'day',
+    WEEK: 'week',
+    MONTH: 'month',
+    QUARTER: 'quarter',
+  };
+
+  const intervalSwitchButtons = [
+    { label: '7 D', value: intervals.DAY },
+    { label: '8 W', value: intervals.WEEK },
+    { label: '6 M', value: intervals.MONTH },
+    { label: '4 Q', value: intervals.QUARTER },
+  ];
+
+  const [selectedInterval, setSelectedInterval] = useState(
+    intervalSwitchButtons[0]
+  );
+  const [activeChart, setActiveChart] = useState(chartSwitchButtons[0]);
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadingError, setLoadingError] = useState(null);
+
+  useEffect(() => {
+    const apiCalls = [];
+    let fetched = [];
+
+    endPoints.forEach((endPoint) => {
+      apiCalls.push(
+        api.get(`${endPoint.url}`, {
+          params: {
+            period: selectedInterval.value,
+          },
+        })
+      );
+    });
+
+    const getDataFromEndpoints = () => {
+      Promise.all(apiCalls)
+        .then((results) => {
+          results.forEach((result) => {
+            fetched.push(result.data[0]);
+          });
+
+          setData(formatData(fetched));
+          setIsLoading(false);
+          setLoadingError(false);
+        })
+        .catch((err) => {
+          if (err.response) {
+            // Not in the 200 response range
+            console.log(err.response.data);
+            console.log(err.response.status);
+            console.log(err.response.headers);
+          } else {
+            console.log(`Error: ${err.message}`);
+          }
+
+          setIsLoading(false);
+          setLoadingError(true);
+        });
+    };
+
+    getDataFromEndpoints();
+  }, [selectedInterval]);
+
+  function formatData(fetchedData) {
+    const data = {};
+
+    data.stats = fetchedData.map((data, index) => {
+      return {
+        label: endPoints[index].name,
+        data: data.data,
+      };
+    });
+    console.log('periods: ' + getPeriods(fetchedData.length));
+    data.labels = getPeriods(fetchedData[0].data.length);
+    console.log('data.labels: ' + data.labels);
+
+    console.log(data);
+
+    return data;
+  }
+
+  function getPeriods(interval) {
+    let periods = [];
+
+    switch (selectedInterval.value) {
+      case intervals.WEEK:
+        periods = dates.getWeeks(interval);
+        break;
+
+      case intervals.MONTH:
+        periods = dates.getMonths(interval);
+        break;
+
+      case intervals.QUARTER:
+        periods = dates.getQuarters(interval);
+        break;
+
+      default:
+        periods = dates.getDays(interval);
+        break;
+    }
+
+    return periods;
+  }
+
+  function onPeriodChange(selectedPeriod) {}
+
+  return (
+    <Card>
+      {isLoading ? (
+        <div style={{ padding: '20px' }}>
+          <Skeleton count={15} />
+        </div>
+      ) : loadingError ? (
+        <div>Error: </div>
+      ) : (
+        <>
+          <Card.Header>
+            <Card.Title title='Sales vs Profit' />
+          </Card.Header>
+          <Card.Header>
+            <ButtonGroup
+              activeButton={activeChart}
+              setActiveButton={setActiveChart}
+              buttons={chartSwitchButtons}
+              onClick={() => {}}
+            />
+
+            <ButtonGroup
+              activeButton={selectedInterval}
+              setActiveButton={setSelectedInterval}
+              buttons={intervalSwitchButtons}
+              onClick={onPeriodChange}
+            />
+          </Card.Header>
+
+          <Card.Body style={{ paddingTop: '2.5vh' }}>
+            {data &&
+              (activeChart.value === charts.BAR ? (
+                <BarChart data={data} />
+              ) : (
+                <LineChart data={data} />
+              ))}
+          </Card.Body>
+        </>
+      )}
+    </Card>
+  );
+}
