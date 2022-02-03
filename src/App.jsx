24e55f4dc082c116331c4a8bd5e1486bf7345f7b
Mohammed Bounaga <medbounaga@gmail.com>
2022-02-03 14:47:10 +0100

    Adding React Router
    react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-import Dashboard from "./pages/Dashboard";
+import { NavBar } from './components';
+import ModalManager from './containers/ModalManager';
+import { Dashboard, Orders } from './pages';
+import './styles/globals.scss';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 
 const App = () => {
-  return <Dashboard />;
+  return (
+    <>
+      <BrowserRouter>
+        <ModalManager />
+        <NavBar />
+        <div className='mainContent'>
+          <Routes>
+            <Route exact path='/' element={<Dashboard />} />
+            <Route path='/orders' element={<Orders />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
