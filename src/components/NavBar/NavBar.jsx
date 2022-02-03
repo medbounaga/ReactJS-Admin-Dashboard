5d9c11efa6ffc6d152cd45ecd683f53437eb152e
Mohammed Bounaga <medbounaga@gmail.com>
2022-02-03 14:43:42 +0100

    responsive NavBar Component

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,16 +1,14 @@
-import styles from "./NavBar.module.scss";
-import { FaBars, FaTimes } from "react-icons/fa";
-import { useState } from "react";
+import styles from './NavBar.module.scss';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 export const NavBar = () => {
-
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-
-    
-    <div className={styles.header}>
-      <div className={styles.logo}><span style={{fontStyle:"italic"}}>Admin</span> Dashboard</div>
+    <div className={styles['nav-bar']}>
+      <div className={styles.logo}>Dashboard</div>
 
       <button
         className={styles.btn}
@@ -22,10 +20,23 @@ export const NavBar = () => {
       </button>
 
       <div className={isOpen ? styles.menu : `${styles.menu} ${styles.slide}`}>
-        <div className={styles.item1}>Home</div>
-        <div className={styles.item2}>About</div>
+        <div className={styles['menu-item']}>
+          <NavLink
+            className={({ isActive }) => (isActive ? styles.active : '')}
+            to='/'
+          >
+            Dashboard
+          </NavLink>
+        </div>
+        <div className={styles['menu-item']}>
+          <NavLink
+            className={({ isActive }) => (isActive ? styles.active : '')}
+            to='/orders'
+          >
+            Orders
+          </NavLink>
+        </div>
       </div>
     </div>
-    
   );
 };
